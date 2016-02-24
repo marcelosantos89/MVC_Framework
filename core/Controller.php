0c919baeab42abeb3be2e28b1fc409a0803e8ab3
Marcelo Santos <marcelosantos89@gmail.com>
2016-02-24 20:55:12 +0000

    New Comments

--- core/Controller.php
+++ core/Controller.php
@@ -1,22 +1,37 @@
 <?php
-
+/**
+ * Manages Controller Class.
+ *
+ * Other controller will extend this Class so they can use Models and Views.
+ */
 class Controller {
 
   /**
-   * @todo Better comments
-   * @todo Check if file_exists before require_once
+   * Loads Model from folder app/models/(file_name).php
+   * @param  [char] $model [name of the Model]
+   * @return [obj]        [Model Object]
    */
-
   public function model($model)
   {
-    require_once '../app/models/'.$model.'.php';
+    if(file_exists('../app/models/'.$model.'.php'))
+      require_once '../app/models/'.$model.'.php';
+    else
+      throw new Exception("Model ".$model." could not be loaded", 1);
+
 
     return new $model();
   }
 
+  /**
+   * Loads View from folder app/views/[(folder_name)/(file_name)].php
+   * @param  [char] $view [name of the Model]
+   */
   public function view($view, $data = [])
   {
-    require_once '../app/views/'.$view.'.php';
+    if(file_exists('../app/views/'.$view.'.php'))
+      require_once '../app/views/'.$view.'.php';
+    else
+      throw new Exception("View ".$view." could not be loaded", 1);
   }
 
 
