0c4f06df723ded4ac3405ed58596c47bc98bbc76
Marcelo Santos <marcelosantos89@gmail.com>
2016-02-24 21:57:34 +0000

    Added bootstrap css, load_css
    added function to load css like the function load_js

--- app/views/Welcome/index.php
+++ app/views/Welcome/index.php
@@ -1,5 +1,8 @@
 <htm>
   <head>
+    <?php echo load_css('bootstrap/css/bootstrap');?>
+    <?php echo load_js('jquery/jquery-1.12.1.min');?>
+    <?php echo load_js('bootstrap/js/bootstrap');?>
   </head>
   <body>
     <h1>Welcome to the MVC_Framework</h1>
--- core/helpers/template.php
+++ core/helpers/template.php
@@ -1,6 +1,5 @@
 <?php
 
-
   /**
   * [Load Javascript Files]
   * @param  [varchar] $location [location of ]
@@ -8,10 +7,29 @@
   */
   function load_js($location, $param = '')
   {
-    if(file_exists('../public/assets/'.$location.'.js')){
-      //return "<script src='assets/".$location.".js'></script>";
+    if(file_exists('../public/assets/'.$location.'.js'))
+    {
       return '<script src="/public/assets/'.$location.'.js"></script>';
-    } else {
+    }
+    else
+    {
       throw new Exception("The Javascript file ".$location." does not exist", 1);
+    }
+  }
+
+  /**
+  * [Load CSS Files]
+  * @param  [varchar] $location [location of ]
+  * @return [string]           [String with location to file]
+  */
+  function load_css($location, $param = '')
+  {
+    if(file_exists('../public/assets/'.$location.'.css'))
+    {
+      return '<link rel="stylesheet" href="/public/assets/'.$location.'.css">';
+    }
+    else
+    {
+      throw new Exception("The CSS file ".$location." does not exist", 1);
     }
   }
